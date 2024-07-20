612ad5fdd8356de6280b0c9f0db8494f6cd0ebcb
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-07-20 18:02:05 +0300

    Add favicon metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,21 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Mara Jewelry",
-  description: "Perfect jewelry made of natural stones",
+  title: 'Mara Jewelry',
+  description: 'Perfect jewelry made of natural stones',
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
