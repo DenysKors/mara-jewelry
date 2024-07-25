e4510104ad92a6d688d33a77be0572d593496878
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-07-25 14:06:17 +0300

    Add hero carousel

--- src/components/HeroCarousel/HeroCarousel.jsx
+++ src/components/HeroCarousel/HeroCarousel.jsx
@@ -1,47 +1,30 @@
 'use client';
 
-import Image from 'next/image';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import styles from './HeroCarousel.module.css';
 import { Carousel } from 'react-responsive-carousel';
-
-import hero1_s from '../../assets/images/hero_bg1@1x_s.jpg';
-import hero2_s from '../../assets/images/hero_bg2@1x_s.jpg';
-import hero3_s from '../../assets/images/hero_bg3@1x_s.jpg';
-import hero4_s from '../../assets/images/hero_bg4@1x_s.jpg';
-import hero5_s from '../../assets/images/hero_bg5@1x_s.jpg';
-import hero6_s from '../../assets/images/hero_bg6@1x_s.jpg';
 
 export default function HeroCarousel() {
   return (
-    <Carousel
-      animationHandler="fade"
-      interval={5000}
-      autoPlay={true}
-      infiniteLoop={true}
-      useKeyboardArrows={false}
-      showStatus={false}
-      showArrows={false}
-      showThumbs={false}
-      swipeable={false}
-    >
-      <div>
-        <Image src={hero1_s} alt="image1" />
-      </div>
-      <div>
-        <Image src={hero2_s} alt="image2" />
-      </div>
-      <div>
-        <Image src={hero3_s} alt="image3" />
-      </div>
-      <div>
-        <Image src={hero4_s} alt="image4" />
-      </div>
-      <div>
-        <Image src={hero5_s} alt="image5" />
-      </div>
-      <div>
-        <Image src={hero6_s} alt="image6" />
-      </div>
-    </Carousel>
+    <>
+      <Carousel
+        animationHandler="fade"
+        interval={5000}
+        autoPlay={true}
+        infiniteLoop={true}
+        useKeyboardArrows={false}
+        showStatus={false}
+        showArrows={false}
+        showThumbs={false}
+        swipeable={false}
+      >
+        <div className={styles.image1}></div>
+        <div className={styles.image2}></div>
+        <div className={styles.image3}></div>
+        <div className={styles.image4}></div>
+        <div className={styles.image5}></div>
+        <div className={styles.image6}></div>
+      </Carousel>
+    </>
   );
 }
