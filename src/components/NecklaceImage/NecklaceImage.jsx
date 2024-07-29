2910024a976434a1925ae9de17b9397c39562a3e
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-07-29 14:30:44 +0300

    Media query fix, catalog container styles fix

--- src/components/NecklaceImage/NecklaceImage.jsx
+++ src/components/NecklaceImage/NecklaceImage.jsx
@@ -34,10 +34,10 @@ export default function NecklaceImage() {
 
   return (
     <picture>
-      <source media="(min-width: 1200px)" srcSet={desktop} />
+      <source media="(min-width: 1440px)" srcSet={desktop} />
       <source media="(min-width: 768px)" srcSet={tablet} />
       <source media="(max-width: 767px)" srcSet={mobile} />
-      <img {...rest} style={{ width: '100%', height: 'auto' }} />
+      <img {...rest} style={{ width: '100%', height: '100%' }} />
     </picture>
   );
 }
