58efb5aa2a5e43a8f91c0440571c18242dc73386
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-10-26 13:58:12 +0300

    Add article form logic

--- src/components/AddArticle/AddArticle.jsx
+++ src/components/AddArticle/AddArticle.jsx
@@ -2,27 +2,36 @@
 
 import Image from 'next/image';
 import * as Yup from 'yup';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 
 import styles from './AddArticle.module.css';
 import imgPlaceholder from '../../../public/no-image-placeholder.png';
 
 const initialValues = {
-  image: null,
   title: '',
-  text: '',
+  parts: [
+    {
+      image: null,
+      text: '',
+    },
+  ],
 };
 
 const articleSchema = Yup.object().shape({
-  image: Yup.string().required("Фото обов'язкове"),
   title: Yup.string()
     .max(50, 'Заголовок занадто довгий')
     .required("Заголовок обов'язковий"),
-  text: Yup.string().required("Зміст обов'язковий"),
+  parts: Yup.array().of(
+    Yup.object().shape({
+      image: Yup.string().required("Фото обов'язкове"),
+      text: Yup.string().required("Зміст обов'язковий"),
+    })
+  ),
 });
 
 export default function AddArticle() {
   const handleSubmit = (values, { resetForm }) => {
+    // async/await AddArticle()
     console.log(values);
     resetForm();
   };
@@ -33,8 +42,11 @@ export default function AddArticle() {
       validationSchema={articleSchema}
       onSubmit={handleSubmit}
     >
-      {({ values, setFieldValue }) => (
+      {({ values, setFieldValue, isSubmitting }) => (
         <Form>
+          <p className={styles.error}>
+            Стаття може складатися з однієї, двох або трьох частин
+          </p>
           <label className={styles.lable}>
             Заголовок:
             <Field
@@ -45,60 +57,93 @@ export default function AddArticle() {
             />
           </label>
           <ErrorMessage className={styles.error} name="title" component="div" />
-          <p className={styles.subTitle}>Фото:</p>
-          <Field name="image">
-            {() => (
-              <input
-                className={styles.input}
-                type="file"
-                accept="image/*,.png, .jpeg, .webm"
-                onChange={event => {
-                  setFieldValue('image', event.currentTarget.files);
-                }}
-              />
+          <FieldArray name="parts">
+            {({ insert, remove, push }) => (
+              <>
+                {values.parts.length > 0 &&
+                  values.parts.map((part, index) => (
+                    <div className={styles.formGroup} key={index}>
+                      <p className={styles.subTitle}>{`Фото частини ${
+                        index + 1
+                      }:`}</p>
+                      <Field name={`parts.${index}.image`}>
+                        {() => (
+                          <input
+                            className={styles.input}
+                            type="file"
+                            accept="image/*,.png, .jpeg, .webp"
+                            onChange={event => {
+                              setFieldValue(
+                                `parts.${index}.image`,
+                                event.currentTarget.files
+                              );
+                            }}
+                          />
+                        )}
+                      </Field>
+                      {part.image ? (
+                        <div className={styles.thumb}>
+                          <Image
+                            className={styles.img}
+                            src={URL.createObjectURL(part.image[0])}
+                            width={335}
+                            height={380}
+                            alt={index}
+                          />
+                        </div>
+                      ) : (
+                        <div className={styles.thumb}>
+                          <Image
+                            className={styles.img}
+                            src={imgPlaceholder}
+                            width={335}
+                            height={380}
+                            alt="No image"
+                            priority
+                          />
+                        </div>
+                      )}
+                      <ErrorMessage
+                        className={styles.error}
+                        name={`parts.${index}.image`}
+                        component="div"
+                      />
+                      <label className={styles.lable}>
+                        {`Зміст частини ${index + 1}:`}
+                        <Field
+                          className={styles.textareaField}
+                          component="textarea"
+                          name={`parts.${index}.text`}
+                          rows="16"
+                        />
+                      </label>
+                      <ErrorMessage
+                        className={styles.error}
+                        name="text"
+                        component="div"
+                      />
+
+                      <div>
+                        <button type="button" onClick={() => remove(index)}>
+                          X
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                <button
+                  type="button"
+                  onClick={() => push({ image: '', text: '' })}
+                >
+                  Add part
+                </button>
+              </>
             )}
-          </Field>
-          <div className={styles.wrapper}>
-            {values.image ? (
-              [...values.image].map((item, idx) => (
-                <div className={styles.thumb} key={idx}>
-                  <Image
-                    className={styles.img}
-                    src={URL.createObjectURL(item)}
-                    width={335}
-                    height={380}
-                    alt={idx}
-                  />
-                </div>
-              ))
-            ) : (
-              <div className={styles.thumb}>
-                <Image
-                  className={styles.img}
-                  src={imgPlaceholder}
-                  width={335}
-                  height={380}
-                  alt="No image"
-                  priority
-                />
-              </div>
-            )}
-          </div>
-          <ErrorMessage className={styles.error} name="image" component="div" />
-          <label className={styles.lable}>
-            Зміст:
-            <Field
-              className={styles.textareaField}
-              component="textarea"
-              name="text"
-              rows="16"
-            />
-          </label>
-          <ErrorMessage className={styles.error} name="text" component="div" />
+          </FieldArray>
           <button
             className={styles.button}
             type="submit"
             aria-label="add article"
+            disabled={isSubmitting}
           >
             Додати статтю
           </button>
