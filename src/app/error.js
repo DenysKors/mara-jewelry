6a309492749b863fb59bbaee54769f4094d256e4
DenysKors <105918404+DenysKors@users.noreply.github.com>
2025-01-15 14:03:50 +0200

    Fix global variable

--- src/app/error.js
+++ src/app/error.js
@@ -4,7 +4,7 @@ import styles from './error.module.css';
 export default function Error({ reset }) {
   return (
     <div className={styles.box}>
-      <h2 className={styles.title}>Щось пішло не так, як хотілось!</h2>
+      <h2 className={styles.title}>Щось пішло не так!</h2>
       <button className={styles.button} onClick={() => reset()}>
         Спробувати знову
       </button>
